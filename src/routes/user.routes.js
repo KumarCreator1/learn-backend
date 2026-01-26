7e16410c3bb70e3c62ed3d9726d4a088c3f8cf43
Vicky Kumar <connect.vickykumar@gmail.com>
2026-01-26 13:50:30 +0530

    feat: add routes for updating avatar and password with JWT verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -25,7 +25,10 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 
+
 //secured route
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/renew-access-token").post(verifyJWT, renewAccessToken);
+router.route("/update-avatar").post(verifyJWT, upload.single("avatar"), updateAvatar);
+router.route("/update-password").post(verifyJWT, updatePassword);
 export default router;
